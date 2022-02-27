e12e55a23c18575704c7af54fa756f0c2aa59581
evgenok <popovevgen18042001@mail.ru>
2022-02-27 23:09:30 +0300

    added slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,12 @@
 $(function() {
-    
-})
+   $('.slider__inner').slick({
+      dots: true,
+      arrows: false,
+      infinite: true,
+      speed: 2000,
+      slidesToShow: 1,
+      autoplay: true,
+      autoplaySpeed: 5000,
+      adaptiveHeight: true
+   });
+});
